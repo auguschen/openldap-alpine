0ac7993325c25a5ee41f18902ffa9785cc457e96
Chen Augus <chenthao@163.com>
2018-08-28 22:06:25 +0800

    change init script path

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 if [ ! -f /etc/openldap/.inited ]; then
   echo "conatiner running up first time."
-  sh /etc/openldap/scripts/initconf.sh && touch /etc/openldap/.inited
+  sh /scripts/initconf.sh && touch /etc/openldap/.inited
 fi
 
 if [ -f /etc/openldap/ssl/cert.pem ] && [ -f /etc/openldap/ssl/key.pem ] && [ -f /etc/openldap/ssl/chain.pem ]; then
